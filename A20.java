72f17d706135093e902fca6c2b8233e75082d1a3
linjinbao666 <linjinbao666@gmail.com>
2022-11-22 18:15:34 +0800

    更新

---
+++ A20.java
@@ -0,0 +1,30 @@
+import java.lang.annotation.Retention;
+import java.rmi.server.RemoteStub;
+import java.util.Stack;
+
+public class A20 {
+    //s = "()[]{}"
+    public boolean isValid(String s) {
+        int length = s.length();
+        if(length%2 !=0) return false;
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<length; i++){
+            Character ch = s.charAt(i);
+            switch (ch) {
+                case '(':
+                    stack.push(')');
+                    break;
+                case '{':
+                    stack.push('}');
+                    break;
+                case '[':
+                    stack.push(']');
+                    break;
+                default:
+                    if(stack.isEmpty() || ch!= stack.pop()) return false;
+                    break;
+            }
+        }
+        return stack.size() %2 ==0;
+    }
+}
--- A242.java
+++ A242.java
@@ -2,17 +2,15 @@ import java.util.*;
 
 public class A242 {
     public boolean isAnagram(String s, String t) {
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
-        for(int i=0; i<s.length(); i++){
-            map1.put(s.charAt(i), map1.getOrDefault(s.charAt(i), 0)+1);
-        }
-        for(int i=0; i<t.length(); i++){
-            map2.put(t.charAt(i), map2.getOrDefault(t.charAt(i), 0)+1);
-        }
-        if(map1.size() != map2.size()) return false;
-        for(Map.Entry<Character, Integer> entry: map1.entrySet()){
-            if(map2.getOrDefault(entry.getKey(), 0) != entry.getValue()) return false;
+        int length1 = s.length();
+        int length2 = t.length();
+        if(length1 != length2) return false;
+        char[] ch1 = s.toCharArray();
+        Arrays.sort(ch1);
+        char[] ch2 = t.toCharArray();
+        Arrays.sort(ch2);
+        for(int i=0; i<length1; i++){
+            if(ch1[i] != ch2[i]) return false;
         }
         return true;
     }
