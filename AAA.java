1072850a7c5a8163364e51cf2ae31f4325aca51c
linjinbao666 <linjinbao666@gmail.com>
2022-12-17 09:34:58 +0800

    modified:   AAA.java

--- AAA.java
+++ AAA.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class AAA {
 
     public String replaceSpace(String s) {
@@ -10,6 +12,15 @@ public class AAA {
             }
         }
         return new String(sb);
+    }
+
+    public String reverseLeftWords(String s, int n) {
+        char[] chars = s.toCharArray();
+        char[] tmp1 = Arrays.copyOfRange(chars, 0, n);
+        char[] tmp2 = Arrays.copyOfRange(chars, n, chars.length);
+        System.arraycopy(tmp2, 0, chars, 0, tmp2.length);
+        System.arraycopy(tmp1, 0, chars, tmp2.length, tmp1.length);
+        return new String(chars);
     }
     
 }
