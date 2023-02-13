2958e86b021db7c7e29784f917b6c5c425de8024
linjinbao666 <linjinbao666@gmail.com>
2023-02-13 11:33:44 +0800

    add B77

---
+++ java/A401.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+import javax.sound.midi.Track;
+
+import java.util.ArrayList;
+
+public class A401 {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> res = new ArrayList<String>();
+
+        return res;
+    }
+
+    public void backTracke(List<String> res, int turnedOn, int index, List<Integer> track){
+        
+    }
+}
---
+++ java/B77.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class B77 {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        backTrack(res, n, k, 1, new ArrayList<Integer>());
+        return res;
+    }
+
+    public void backTrack(List<List<Integer>> res, int n, int k, int index, List<Integer> track){
+        if(track.size() == k) {
+            res.add(new ArrayList<>(track));
+            return ;
+        }
+
+        for(int i=index; i<=n; i++){
+            if(track.contains(i)) continue;
+            track.add(i);
+            index = index +1;
+            backTrack(res, n, k, index, track);
+            track.remove(track.size()-1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new B77().combine(3, 3);
+    }
+}
