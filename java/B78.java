e28395925d17ca4e78d732c02136c3087dad3702
linjinbao666 <linjinbao666@gmail.com>
2023-02-15 18:27:37 +0800

    update

---
+++ java/B78.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class B78 {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i=0; i<nums.length; i++){
+            res.addAll(help(nums, i));
+        }
+        return res;
+    }
+    
+    public List<List<Integer>> help(int[] nums, int k){
+        List<List<Integer>> res = new ArrayList<>();
+        backTrack(res, nums, 0, k, new ArrayList<>());
+        return res;
+    }
+
+    public void backTrack(List<List<Integer>> res, int[] nums, int index, int k, List<Integer> track){
+        if(track.size() == k){
+            res.add(new ArrayList<>(track));
+            return ;
+        }
+        for(int i=index; i<nums.length; i++){
+            if(track.contains(nums[i])) continue;
+            track.add(nums[i]);
+            backTrack(res, nums, index+1, k, track);
+            track.remove(track.size()-1);
+        }
+    }
+}
