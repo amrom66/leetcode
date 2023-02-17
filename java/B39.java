c9f993f6aa11d6911a649c6cae252ebce1b701b4
linjinbao666 <linjinbao666@gmail.com>
2023-02-17 10:03:25 +0800

    B39.java

---
+++ java/B39.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class B39 {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        backTrack(res, candidates, 0, target, new ArrayList<>());
+        return res;
+    }
+
+    public void backTrack(List<List<Integer>> res, int[] candidates, int index, int target, List<Integer> track){
+        Integer sum = 0;
+        for(Integer num : track) sum += num;
+        if(sum == target) {
+            res.add(new ArrayList<>(track));
+        }
+        if(sum > target){
+            return ;
+        }
+        for(int i=index; i<candidates.length; i++){
+            track.add(candidates[i]);
+            backTrack(res, candidates, index, target, track);
+            index ++;
+            track.remove(track.size()-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] candidates = {2,3,6,7};
+        int target = 7;
+        List<List<Integer>> res = new B39().combinationSum(candidates, target);
+        res.stream().forEach(re ->{
+            re.stream().forEach(r ->{
+                System.out.print(r + "\t");   
+            });
+            System.out.println();
+        });
+    }
+    
+}
