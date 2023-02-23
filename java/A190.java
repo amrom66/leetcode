0725ec0428ae6d8c5487e88944dad1c856bd2e9c
linjinbao666 <linjinbao666@gmail.com>
2023-02-23 14:12:14 +0800

    update

---
+++ java/A190.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class A190 {
+    public int reverseBits(int n) {
+        int sum = 0;
+        for(int i=0; i<32 && n != 0; i++){
+           
+        }
+    }
+}
