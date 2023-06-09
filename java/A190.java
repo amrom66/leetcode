b571959e9220faef976b0960611b8ffe33f98f47
linjinbao66 <linjinbao66@gmail.com>
2023-06-09 18:39:23 +0800

    sss

--- java/A190.java
+++ java/A190.java
@@ -1,11 +1,6 @@
-import java.util.Arrays;
-
 public class A190 {
     public int reverseBits(int n) {
-        int sum = 0;
-        for(int i=0; i<32 && n != 0; i++){
-           
-        }
+
         return 0;
     }
 }
--- java/A3.java
+++ java/A3.java
@@ -5,10 +5,10 @@ public class A3 {
     public int lengthOfLongestSubstring(String s) {
         Map<Character, Integer> map = new HashMap<>();
         int len = 0;
-        
-        for(int i=0; i<s.length(); i++){
-            if(map.containsKey(s.charAt(i))){
-                len = Math.max(len, i-map.get(s.charAt(i)));
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                len = Math.max(len, i - map.get(s.charAt(i)));
             }
             map.put(s.charAt(i), i);
         }
