725ea7f6afabc9e23d1be06bc550c7487cab0eb9
linjinbao66 <linjinbao66@gmail.com>
2024-02-27 21:32:42 +0800

    从大到小排序

--- java/Sort.java
+++ java/Sort.java
@@ -1,10 +1,45 @@
 public class Sort {
-    public static void quickSort(int[] nums) {
+    public void quickSort(int[] nums) {
         int length = nums.length;
         quickSort(nums, 0, length - 1);
     }
 
-    public static void quickSort(int[] nums, int start, int end) {
+    // 从大到小排序
+    public void quickSort2(int[] nums) {
+        int length = nums.length;
+        quickSort2(nums, 0, length - 1);
+    }
+
+    // 从大到小排序
+    public void quickSort2(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int left = start;
+        int right = end;
+        int pivot = nums[left];
+        while (left < right) {
+            while (left < right && nums[right] < pivot) {
+                right--;
+            }
+            if (left < right) {
+                nums[left] = nums[right];
+                left++;
+            }
+            while (left < right && nums[left] > pivot) {
+                left++;
+            }
+            if (left < right) {
+                nums[right] = nums[left];
+                right--;
+            }
+        }
+        nums[left] = pivot;
+        quickSort2(nums, start, left - 1);
+        quickSort2(nums, left + 1, end);
+    }
+
+    public void quickSort(int[] nums, int start, int end) {
         if (start >= end) {
             return;
         }
@@ -41,7 +76,7 @@ public class Sort {
             System.out.printf("%d ", a[i]);
         System.out.printf("\n");
 
-        quickSort(a);
+        new Sort().quickSort2(a);
 
         System.out.printf("after  sort:");
         for (i = 0; i < a.length; i++)
