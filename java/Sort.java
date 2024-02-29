7f4375dc5cf3b0a8ebf4aa60dc5ed26f2b5d7ca6
linjinbao66 <linjinbao66@gmail.com>
2024-02-29 22:09:03 +0800

    mergeSort

--- java/Sort.java
+++ java/Sort.java
@@ -1,5 +1,47 @@
 public class Sort {
 
+    // 30, 40, 60, 10, 20, 50
+    public void mergeSort(int[] nums) {
+        int length = nums.length;
+        mergeSort(nums, 0, length - 1);
+    }
+
+    public void mergeSort(int[] nums, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = left + (right - left) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+        merge(nums, left, mid, right);
+    }
+
+    public void merge(int[] nums, int left, int mid, int right) {
+        int[] tmp = new int[right - left + 1];
+        int index1 = left;
+        int index2 = mid + 1;
+        int index = 0;
+        while (index1 <= mid && index2 <= right) {
+            if (nums[index1] <= nums[index2]) {
+                tmp[index] = nums[index1];
+                index1++;
+            } else {
+                tmp[index] = nums[index2];
+                index2++;
+            }
+            index++;
+        }
+        while (index1 <= mid) {
+            tmp[index++] = nums[index1++];
+        }
+        while (index2 <= right) {
+            tmp[index++] = nums[index2++];
+        }
+        for (int i = 0; i < tmp.length; i++) {
+            nums[left + i] = tmp[i];
+        }
+    }
+
     public void insertSort2(int[] nums) {
         int length = nums.length;
         for (int i = 1; i < length; i++) {
@@ -134,7 +176,7 @@ public class Sort {
             System.out.printf("%d ", nums[i]);
         System.out.printf("\n");
 
-        new Sort().insertSort2(nums);
+        new Sort().mergeSort(nums);
 
         System.out.printf("after  sort:");
         for (i = 0; i < nums.length; i++)
